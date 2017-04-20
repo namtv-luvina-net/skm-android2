a1c280143b9966b6a0ab3a51e2b88f3164fcf4a0
Nguyen Duc Dat <luongdolong@gmail.com>
2017-04-20 11:09:09 +0700

    + fix bug 170

--- app/src/main/java/jp/co/soliton/keymanager/common/InfoDevice.java
+++ app/src/main/java/jp/co/soliton/keymanager/common/InfoDevice.java
@@ -161,7 +161,7 @@ public class InfoDevice {
 
 	private String sizeToFormat(long size) {
 		if(size <= 0) return "0";
-		final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
+		final String[] units = new String[] { "", "kB", "MB", "GB", "TB" };
 		int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
 		return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + units[digitGroups];
 	}
