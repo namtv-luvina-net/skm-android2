8d440504ba6b493dfa2ba10b9d94fc3f3e8a624a
Nguyen Duc Dat <luongdolong@gmail.com>
2018-07-06 16:05:52 +0700

    + fix bug 287 - redID 653

--- app/src/main/java/jp/co/soliton/keymanager/alarm/AlarmReceiver.java
+++ app/src/main/java/jp/co/soliton/keymanager/alarm/AlarmReceiver.java
@@ -1,6 +1,7 @@
 package jp.co.soliton.keymanager.alarm;
 
 import android.app.AlarmManager;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -8,6 +9,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.support.v4.app.NotificationCompat;
@@ -20,14 +23,12 @@ import jp.co.soliton.keymanager.common.DateUtils;
 import jp.co.soliton.keymanager.dbalias.ElementApply;
 import jp.co.soliton.keymanager.dbalias.ElementApplyManager;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
-    final public static String ONE_TIME = "onetime";
     @Override
     public void onReceive(Context context, Intent intent) {
 	    boolean isTablet = context.getResources().getBoolean(R.bool.isTablet);
@@ -39,44 +40,61 @@ public class AlarmReceiver extends BroadcastReceiver {
         //You can do the processing here update the widget/remote views.
         Bundle extras = intent.getExtras();
 
-        String id = extras.getString(StringList.ELEMENT_APPLY_ID, "");
-	    ElementApplyManager mgr = ElementApplyManager.getInstance(context);
-        ElementApply element = mgr.getElementApply(id);
-	    Bitmap bmLarge = getLargeIcon(context);
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(context)
-		                .setLargeIcon(bmLarge)
-                        .setSmallIcon(getNotificationIcon())
-		                .setColor(context.getResources().getColor(R.color.product_icon_notification))
-                        .setContentTitle(context.getString(R.string.notif_title))
-                        .setContentText(element.getcNValue());
-        Intent resultIntent;
-	    if (isTablet) {
-		    resultIntent = new Intent(context, MenuAcivity.class);
-	    }else {
-		    resultIntent = new Intent(context, AlarmReapplyActivity.class);
-	    }
-	    resultIntent.putExtra(StringList.ELEMENT_APPLY_ID, id);
-	    TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-	    if (!isTablet) {
-		    stackBuilder.addParentStack(AlarmReapplyActivity.class);
-	    }
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(
-                        Integer.parseInt(id),
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        mBuilder.setContentIntent(resultPendingIntent);
-        mBuilder.setAutoCancel(true);
-        NotificationManager mNotificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        final int _id = (int) System.currentTimeMillis();
-	    mNotificationManager.notify(_id, mBuilder.build());
+	    String id = extras.getString(StringList.ELEMENT_APPLY_ID, "");
+		ElementApplyManager mgr = ElementApplyManager.getInstance(context);
+		ElementApply element = mgr.getElementApply(id);
 
-        //Release the lock
-        wl.release();
-    }
+		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context
+				.NOTIFICATION_SERVICE);
+		String NOTIFICATION_CHANNEL_ID = "jp.co.soliton.keymanager.ID";
+		String NOTIFICATION_CHANNEL_NAME = "jp.co.soliton.keymanager.NAME";
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,
+					NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+			// Configure the notification channel.
+			notificationChannel.setDescription("Channel description");
+			notificationChannel.enableLights(true);
+			notificationChannel.setLightColor(Color.RED);
+			notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+			notificationChannel.enableVibration(true);
+			notificationManager.createNotificationChannel(notificationChannel);
+		}
+
+		Bitmap bmLarge = getLargeIcon(context);
+
+		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID);
+
+		mBuilder.setLargeIcon(bmLarge)
+				.setSmallIcon(getNotificationIcon())
+				.setColor(context.getResources().getColor(R.color.product_icon_notification))
+				.setContentTitle(context.getString(R.string.notif_title))
+				.setContentText(element.getcNValue());
+		Intent resultIntent;
+		if (isTablet) {
+			resultIntent = new Intent(context, MenuAcivity.class);
+		} else {
+			resultIntent = new Intent(context, AlarmReapplyActivity.class);
+		}
+		resultIntent.putExtra(StringList.ELEMENT_APPLY_ID, id);
+		TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+		if (!isTablet) {
+			stackBuilder.addParentStack(AlarmReapplyActivity.class);
+		}
+		stackBuilder.addNextIntent(resultIntent);
+		PendingIntent resultPendingIntent =
+				stackBuilder.getPendingIntent(
+						Integer.parseInt(id),
+						PendingIntent.FLAG_UPDATE_CURRENT
+				);
+		mBuilder.setContentIntent(resultPendingIntent);
+		mBuilder.setAutoCancel(true);
+		NotificationManager mNotificationManager =
+				(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		final int _id = (int) System.currentTimeMillis();
+		mNotificationManager.notify(_id, mBuilder.build());
+		//Release the lock
+		wl.release();
+	}
 
 	private int getNotificationIcon() {
 		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
@@ -84,26 +102,6 @@ public class AlarmReceiver extends BroadcastReceiver {
 		}
 		return R.mipmap.ic_notification;
 	}
-
-    public void setOnetimeTimer(Context context, String elementId){
-
-        try {
-            AlarmManager am=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-            Intent intent = new Intent(context, AlarmReceiver.class);
-            intent.putExtra(StringList.ELEMENT_APPLY_ID, elementId);
-            final int _id = (int) System.currentTimeMillis();
-            PendingIntent pi = PendingIntent.getBroadcast(context, _id, intent, PendingIntent.FLAG_ONE_SHOT);
-            ElementApplyManager mgr = ElementApplyManager.getInstance(context);
-            ElementApply element = mgr.getElementApply(elementId);
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-            Date expirationDate = formatter.parse(element.getExpirationDate());
-
-            am.set(AlarmManager.RTC_WAKEUP, expirationDate.getTime(), pi);
-//            am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 10000, pi);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     private void cancelNotification(Context context) {
         NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
@@ -137,12 +135,14 @@ public class AlarmReceiver extends BroadcastReceiver {
     }
 
 	private void setAlarm(Context context, String value, int requestCode, long time) {
-		AlarmManager am =(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+		AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 		Intent intent = new Intent(context, AlarmReceiver.class);
 		intent.removeExtra(StringList.ELEMENT_APPLY_ID);
 		intent.putExtra(StringList.ELEMENT_APPLY_ID, value);
-		PendingIntent pi = PendingIntent.getBroadcast(context, requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-		am.set(AlarmManager.RTC_WAKEUP, time, pi);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, requestCode, intent, PendingIntent
+				.FLAG_UPDATE_CURRENT);
+		am.cancel(pendingIntent);
+		am.set(AlarmManager.RTC_WAKEUP, time, pendingIntent);
 	}
 
 	public Bitmap getLargeIcon(Context context) {
